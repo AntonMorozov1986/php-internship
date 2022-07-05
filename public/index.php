206d6c6bc915667c9578e8e5faa1e0c0db25b7aa
Anton Morozov <amorozov86@gmail.com>
2022-07-05 18:54:50 +0300

    init composer

--- public/index.php
+++ public/index.php
@@ -1,36 +1,57 @@
 <?php
-//phpinfo();
-//require_once dirname(__DIR__) . '/app/init_app.php';
+function getTableRow($userData)
+{
+    $columnData = explode(',', $userData);
 
-$driver = 'mysql'; // $_ENV['DB_DRIVER'];
-$host = 'mysql'; // $_ENV['DB_HOST'];
-$name = 'db-php-internship'; // $_ENV['DB_NAME'];
-$DSN = "{$driver}:host={$host};dbname={$name}";
-
-$dbUser = 'user'; // $_ENV['DB_USER'];
-$dbPass = 'pass'; // $_ENV['DB_PASS']
-
-$pdo = new PDO($DSN, $dbUser, $dbPass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-$query = $pdo->query('SHOW VARIABLES like "version"');
-
-$row = $query->fetch();
-
-echo 'MySQL version:' . $row['Value'];
-?>
-<h3>Проверка работы php</h3>
-<p>Если вы видите версию mySQL, то это значит, что все запущено успешно. &#128640; &#127881;</p>
-<hr>
-<h3>Информация о PHP</h3>
-<details>
-    <summary><b>phpinfo</b></summary>
-    <?php phpinfo()?>
-</details>
-<a class="link" href="/">&#9166; Вернуться на главную</a>
-<style>
-    .link {
-        text-decoration: none;
-        font: inherit;
-        color: inherit;
+    if (sizeof($columnData) < 4) {
+        return '';
     }
-</style>
+
+    $row = '<tr>';
+    foreach ($columnData as $data) {
+        $row .= "<th scope=\"col\">$data</th>";
+    }
+    $row .= '</tr>';
+    return $row;
+}
+
+function getTableBody($tableData)
+{
+    $tableBody = '<tbody>';
+    foreach ($tableData as $row) {
+        $tableBody .= getTableRow($row);
+    }
+    $tableBody .= '</tbody>';
+    return $tableBody;
+}
+
+$usersList = explode(';', file_get_contents('./data'));
+$tbody = getTableBody($usersList)
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Users</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+<div class="container pt-5">
+    <h1>Users Table</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">id</th>
+            <th scope="col">email</th>
+            <th scope="col">name</th>
+            <th scope="col">role</th>
+        </tr>
+        </thead>
+        <?=$tbody?>
+    </table>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
